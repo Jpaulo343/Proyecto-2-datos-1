17ca7261f4127801925e9528e7f1059881178b6e
Jean Paulo Poveda Chaves <J.poveda.1@estudiantec.cr>
2025-11-14 19:04:00 -0600

    chore(godot): agregar solución inicial de proyecto con soporte C#

---
+++ arbol-genealogico/scripts/GlobalData.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class GlobalData : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+			public static int X = 0; //borrar cuando se añadan datos
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
