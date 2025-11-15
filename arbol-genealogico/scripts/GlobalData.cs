68f7bc16a872b7d9c246301bb5cae3ec60a103e4
Jean Paulo Poveda Chaves <J.poveda.1@estudiantec.cr>
2025-11-14 22:59:01 -0600

    fix(Clase globals): se corrigió un error en al clase global data

--- arbol-genealogico/scripts/GlobalData.cs
+++ arbol-genealogico/scripts/GlobalData.cs
@@ -3,14 +3,5 @@ using System;
 
 public partial class GlobalData : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-			public static int X = 0; //borrar cuando se añadan datos
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+	public static int X = 0; //borrar cuando se añadan datos
 }
